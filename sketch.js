e5fa01d7ebd1e16058c75a3ba377e2bddcec4e4f
Morphylus <martindefuns@kns.ch>
2023-02-14 20:54:24 +0100

    Implemented correctly now, still some bugs to fix

--- sketch.js
+++ sketch.js
@@ -1,46 +1,47 @@
 let point_x = 0;
 let point_y = 0;
 let point_set = false;
-const HEIGHT = 500;
 const WIDTH = 500;
-const DIAG = Math.round(Math.sqrt(HEIGHT*HEIGHT + WIDTH * WIDTH));
-let grid = new Array(HEIGHT);
+const HEIGHT = 500;
+const DIAG = Math.round(Math.sqrt(WIDTH*WIDTH + HEIGHT * HEIGHT));
+let grid = new Array(DIAG);
 
-for(let i = 0; i < HEIGHT; i++) {
-    grid[i] = new Array(WIDTH);
+for(let i = 0; i < DIAG; i++) {
+    grid[i] = new Array(HEIGHT);
     grid[i].fill(0);
 }
 
 const sketch = p => {
 
     p.setup = function() {
-        p.createCanvas(HEIGHT,WIDTH);
+        p.createCanvas(WIDTH,HEIGHT);
         p.background(200);
         p.fill(0);
 
         // Reset Button
         button = p.createButton('reset');
-        button.position(WIDTH/2,HEIGHT);
+        button.position(HEIGHT/2,WIDTH);
         button.mousePressed(clearScreen);
 
     }
 
     p.mousePressed = function() {
-        if(p.mouseX > HEIGHT || p.mouseY > WIDTH) return;
+        if(p.mouseX > WIDTH || p.mouseY > HEIGHT) return;
 
         p.noStroke();
         p.ellipse(p.mouseX, p.mouseY, 5, 5);
             if(!point_set) {
-                point_x = p.mouseX - WIDTH/2;
-                point_y = p.mouseY - HEIGHT/2;
+                point_x = p.mouseX - HEIGHT/2;
+                point_y = p.mouseY - WIDTH/2;
                 point_set = true;
+                checkForLines();
             }
     }
 }
 
 const sineWave = p => {
     p.setup = function() {
-        p.createCanvas(WIDTH, DIAG);
+        p.createCanvas(HEIGHT, DIAG);
         p.background(0);
     }
 
@@ -48,7 +49,6 @@ const sineWave = p => {
         if(point_set) {
             drawWave(this, point_x, point_y);
             point_set = false;
-            checkForLines();
         }
     }
 
@@ -56,6 +56,7 @@ const sineWave = p => {
 
 let drawingCanvas = new p5(sketch);
 let sin = new p5(sineWave);
+
 
 // Draw line given angle and radius - could be done easier if I was better at Linalg lol
 function drawLine(p, r, theta) {
@@ -65,7 +66,7 @@ function drawLine(p, r, theta) {
         let dy = Math.sin(-theta + Math.PI/2);
         let norm = Math.sqrt(dx*dx + dy*dy);
         let orthLine = [dx / norm, dy / norm];
-        p.line(-orthLine[0] * WIDTH + WIDTH/2, -orthLine[1] * HEIGHT + HEIGHT/2, orthLine[0] * WIDTH + WIDTH/2, orthLine[1] * HEIGHT + HEIGHT/2);
+        p.line(-orthLine[0] * HEIGHT + HEIGHT/2, -orthLine[1] * WIDTH + WIDTH/2, orthLine[0] * HEIGHT + HEIGHT/2, orthLine[1] * WIDTH + WIDTH/2);
         return;
     }
 
@@ -73,15 +74,15 @@ function drawLine(p, r, theta) {
     let dy = r * Math.sin(-theta);
     let norm = Math.sqrt(dx*dx + dy*dy);
     let orthLine = [-dy / norm, dx / norm];
-    p.line((dx - orthLine[0] * WIDTH) + WIDTH/2, (dy - orthLine[1] * HEIGHT) + HEIGHT/2, (dx + orthLine[0] * WIDTH) + WIDTH/2, (dy + orthLine[1] * HEIGHT) + HEIGHT/2);
+    p.line((dx - orthLine[0] * HEIGHT) + HEIGHT/2, (dy - orthLine[1] * WIDTH) + WIDTH/2, (dx + orthLine[0] * HEIGHT) + HEIGHT/2, (dy + orthLine[1] * WIDTH) + WIDTH/2);
 }
 
 // Calculate wave values (discrete)
 function calcWave(x, y) {
-    let inc = Math.PI / WIDTH;
-    yvalues = new Array(WIDTH);
+    let inc = Math.PI / HEIGHT;
+    yvalues = new Array(HEIGHT);
     let dx = 0.0;
-    for(let i = 0; i < WIDTH; i++) {
+    for(let i = 0; i < HEIGHT; i++) {
         yvalues[i] = Math.round(x * Math.cos(dx) - y * Math.sin(dx));
         dx += inc;
     }
@@ -93,18 +94,20 @@ function drawWave(p, x, y) {
     p.noStroke();
     p.fill(255);
     let wave = calcWave(x,y);
-    for(let i = 0; i < WIDTH; i++) {
+    for(let i = 0; i < HEIGHT; i++) {
         p.noStroke();
         p.ellipse(i,wave[i] + DIAG/2, 1, 1);
-        grid[i][wave[i]+DIAG] += 1;
+        grid[wave[i] + HEIGHT/2][i] += 1;
     }
 }
 
 function checkForLines() {
-    for(let i = 0; i < HEIGHT; i++) {
-        for(let j = 0; j < WIDTH; j++) {
+    console.log("Here");
+    for(let i = 0; i < DIAG; i++) {
+        for(let j = 0; j < HEIGHT; j++) {
             if (grid[i][j] > 2) {
-                drawLine(drawingCanvas, i - HEIGHT/2, 2*Math.PI / WIDTH * j);
+                console.log("Drawn!");
+                drawLine(drawingCanvas, i - HEIGHT/2, Math.PI/HEIGHT * j);
                 grid[i][j] = 0;
                 return;
             }
